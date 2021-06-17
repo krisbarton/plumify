77dcbd87d4f11c3e483ccb44d6f4a6a5b8904405
Kris Barton <krisbarton@gmail.com>
2021-06-17 10:14:18 +0100

    Creating initial react set up

--- src/api/search.js
+++ src/api/search.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
 
-export default {
-    /**
+/**
      * @param query
      * @returns {Promise<AxiosResponse<T>>}
      */
-    search(song) {
-      return axios.get(`https://deezerdevs-deezer.p.rapidapi.com/search?q=track:${song}`, {
-          'headers': {
-              'x-rapidapi-host': API_HOST,
-              'x-rapidapi-key': API_KEY,
-          }
-      });
-    },
+
+const doSearch = (song) => {
+
+    return axios.get(`https://deezerdevs-deezer.p.rapidapi.com/search?q=track:${song}`, {
+        'headers': {
+            'x-rapidapi-host': API_HOST,
+            'x-rapidapi-key': API_KEY,
+        }
+    });
+
 }
+
+export default doSearch;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import '../index.html';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
